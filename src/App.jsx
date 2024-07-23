3cc716d7cc899864770249621697aa826c86484f
Rajat93gupta <rajatkumr3991@gmail.com>
2024-07-23 11:53:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Demo from './component/Demo'
+import Hero from './component/Hero'
+
+const  App =()=> {
+ 
+
+  return (
+    <main>
+      <div className='main'>
+        <div className='gradient'/>
+      </div>
+      <div className='app'>
+        <Hero/>
+        <Demo/>
+      </div>
+     
+     
+    </main>
+  )
+}
+
+export default App
---
+++ src/Services/Article.js
@@ -0,0 +1,25 @@
+ import {createApi , fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+
+ const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+ console.log(rapidApiKey);
+ export const articleApi = createApi({
+    reducerPath:'articleApi',
+    baseQuery:fetchBaseQuery({
+        baseUrl:'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders:(headers)=>{
+            headers.set('x-rapidapi-host','article-extractor-and-summarizer.p.rapidapi.com'),
+            headers.set('x-rapidapi-key',rapidApiKey)
+
+            return headers;
+        }
+
+    }),
+    endpoints:(builder=>({
+        getSummary:builder.query({
+            query:(params)=>`/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
+        })
+    }))
+ })
+
+
+ export const {useLazyGetSummaryQuery} =articleApi;
